122ff49c95cc83714f5cbb8ea298eb881aecbd9d
Samantha Koh <philip.koh@techs-iMac-3.local>
2020-02-28 16:20:32 +0800

    standardise naming of 'username' variable

--- backend/src/controllers/postController.js
+++ backend/src/controllers/postController.js
@@ -2,7 +2,7 @@ import { Post, Wall } from "../db/models";
 
 export const createPost = async (req, res) => {
   const [newPost, associatedWall] = await Promise.all([
-    Post.create({ value: req.body.value, username: req.body.signedInUsername }),
+    Post.create({ value: req.body.value, username: req.body.username }),
     Wall.findOne({ where: { name: req.body.wallName } })
   ]);
   await newPost.setWall(associatedWall);
